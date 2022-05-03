b5278c05013c8a2ad56e65c87a89d729765a4c36
Narayan Gaonkar <narayan.gaonkar@softlabsgroup.com>
2022-05-03 19:36:40 +0530

    Contract Project Manager Class and DB Context

---
+++ WebAPI/CPPWebService/WebAPI/Models/ContractProjectManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+using WebAPI.Helper;
+using System.Diagnostics;
+
+namespace WebAPI.Models
+{
+    [Table("contract_project_manager")]
+    public class ContractProjectManager
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProgramId { get; set; }
+        public bool IsMailSent { get; set; }
+
+
+        // Narayan - Send Mail To Project Manager from Contract
+        public static void SendSetUpMail(int ContractID, int id, string ModifiedValue)
+        {
+            try
+            {
+                using (var ctx = new CPPDbContext())
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
