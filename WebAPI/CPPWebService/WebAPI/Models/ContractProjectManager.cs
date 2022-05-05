0013fcfa2b64ae5e748e9d2840c71aed0055105f
Narayan Gaonkar <narayan.gaonkar@softlabsgroup.com>
2022-05-05 21:09:11 +0530

    contract manager mail service and method

--- WebAPI/CPPWebService/WebAPI/Models/ContractProjectManager.cs
+++ WebAPI/CPPWebService/WebAPI/Models/ContractProjectManager.cs
@@ -19,12 +19,25 @@ namespace WebAPI.Models
 
 
         // Narayan - Send Mail To Project Manager from Contract
-        public static void SendSetUpMail(int ContractID, int id, string ModifiedValue)
+        public static void SendSetUpMail(int ContractID)
         {
             try
             {
                 using (var ctx = new CPPDbContext())
                 {
+                    List<ContractProjectManager> contractProjectManagers = ctx.ContractProjectManagers.Where(c => c.ProgramId == ContractID).ToList();
+                    Program contract = ctx.Program.Where(c => c.ProgramID == ContractID).FirstOrDefault();
+                    for (int i = 0; i < contractProjectManagers.Count; i++)
+                    {
+                        if (contractProjectManagers[i].IsMailSent != true)
+                        {
+                            int userID = contractProjectManagers[i].UserId;
+                            User user = ctx.User.Where(u => u.Id == userID).FirstOrDefault();
+                            WebAPI.Services.MailServices.ContractProjectManagerMail(user.Email, contract.ContractNumber, contract.ProgramName);
+                            contractProjectManagers[i].IsMailSent = true;
+                        }
+                    }
+                    ctx.SaveChanges();
                 }
             }
             catch (Exception ex)
