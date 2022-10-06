93d9db67a5d1cbc03093ddae5b87c4b0dd428e6b
Narayan Gaonkar <narayan.gaonkar@softlabsgroup.com>
2022-10-06 11:05:11 +0530

    trend section fixes for add trend and duplicate entry

--- CPP-FrontEnd/CPP-FrontEnd/app/controllers/WBSCtrl.js
+++ CPP-FrontEnd/CPP-FrontEnd/app/controllers/WBSCtrl.js
@@ -5254,13 +5254,13 @@ angular.module('cpp.controllers').
                                 var projectElement = selProject.children[projectElementI];
                                 //Aditya :: for keeping the element selected after save :: 27092022 
                                 if (selElementID != undefined && selElementID == projectElement.ProjectID && localStorage.getItem('MODE') == 'gridview') {
-                                    _selectedProjectID = selElementID;
+                                    wbsTree.setSelectedProjectID(selElementID);
                                     selectedElement = projectElement;
                                     strElement += "<tr class='selected contact-row' id=" + selElementID + ">";
                                     getTrendGridSection(projectElement);
                                 }
                                 else if (projectElementI == 0 && selElementID == undefined) {
-                                    _selectedProjectID = projectElement.ProjectID;
+                                    wbsTree.setSelectedProjectID(projectElement.ProjectID);
                                     selectedElement = projectElement;
                                     strElement += "<tr class='selected contact-row' id=" + projectElement.ProjectID + ">";
                                     getTrendGridSection(projectElement);
@@ -5372,6 +5372,7 @@ angular.module('cpp.controllers').
                                 _baseline = response.data.result.FutureTrendList[0];
                             }
                             strTrend = "";
+                            $('#tblTrend tbody').html('');
 
                             strTrend += "<tr class='contact-row'>";
                             strTrend += "<td><a href=" + "#/app/cost-gantt/" + selectedProjectElementID + "/0/" + orgId + ">" + _baseline.TrendDescription + "</td>";
@@ -6037,6 +6038,9 @@ angular.module('cpp.controllers').
                 }
 
                 function BindTrendEvent(selectedProgramID, selectedProjectID, selectedElementID) {
+
+                    wbsTree.getWBSTrendTree().trendGraph(true);
+
                     //Add Trend
                     $("#AddTrendGridBtn").unbind('click').on("click", function () {
                         //var programId = $scope.GridContractId;
@@ -6052,7 +6056,6 @@ angular.module('cpp.controllers').
                         }
                         wbsTree.setNewTrend(true);
                         var s = wbsTree.getWBSTrendTree().getTrendNumber();
-                        wbsTree.getWBSTrendTree().trendGraph(true);
                         wbsTree.setSelectedNode(selectedElement);
                         var allElementTrendData = $scope.gridTrendData;
                         var selectedTrend = {};
