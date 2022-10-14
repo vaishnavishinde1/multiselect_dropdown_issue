17c35934b8e591479aaa9bae979542e58936c21a
Narayan Gaonkar <narayan.gaonkar@softlabsgroup.com>
2022-10-14 20:57:45 +0530

    backend controllers and modals for approval master

---
+++ WebAPI/CPPWebService/WebAPI/Controllers/RegisterAdminApprovalController.cs
@@ -0,0 +1,102 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Web.Http;
+
+using WebAPI.Models;
+
+
+namespace WebAPI.Controllers
+{
+    public class RegisterAdminApprovalController : ApiController
+    {
+        //
+        // GET: /RegisterUser/
+        public HttpResponseMessage Post([FromBody] List<AdminApproval> adminApprovalList)
+        {
+
+            String status = "";
+            foreach (var AdminApproval in adminApprovalList)
+            {
+                switch (AdminApproval.Operation)
+                {
+                    case 1:
+                        status += WebAPI.Models.AdminApproval.RegisterNewApprover(AdminApproval);
+                        break;
+                    case 2:
+                        status += WebAPI.Models.AdminApproval.UpdateApprover(AdminApproval);
+                        break;
+                    case 3:
+                        status += WebAPI.Models.AdminApproval.DeleteApprover(AdminApproval);
+                        break;
+                    case 4:
+                        status += "";
+                        break;
+                }
+
+            }
+            var jsonNew = new
+            {
+                result = status
+            };
+            return Request.CreateResponse(HttpStatusCode.OK, jsonNew);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    {
+//        String status = "";
+//            foreach (var admin_approval in admin_approval)
+//            {
+//                switch (admin_approval.Operation)
+//                {
+//                    case 1:
+//                        status += WebAPI.Models.admin_approval.add(admin_approval);
+//                        break;
+//                    case 2:
+//                        status += WebAPI.Models.admin_approval.update(admin_approval);
+//                        break;
+//                    case 3:
+//                        status += WebAPI.Models.admin_approval.delete(admin_approval);
+//                        break;
+//                    case 4:
+//                        status += "";
+//                        break;
+//                }
+//    //i
+//}
+//var jsonNew = new
+//{
+//    result = status
+//};
+//return Request.CreateResponse(HttpStatusCode.OK, jsonNew);
+//        }
+//    }
+//}
---
+++ WebAPI/CPPWebService/WebAPI/Models/AdminApprovalPresident.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+using WebAPI.Helper;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAPI.Models
+{
+    [Table("admin_approval_president")]
+    public class AdminApprovalPresident : Audit
+    {
+        [NotMapped]
+        public int Operation;
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public int PresidentID { get; set; }
+
+        public static AdminApprovalPresident GetFirstDefaultPresident()
+        {
+            Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "Entry Point", Logger.logLevel.Info);
+            Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "", Logger.logLevel.Debug);
+
+            AdminApprovalPresident adminApprovalPresidents = new AdminApprovalPresident();
+            using (var ctx = new CPPDbContext())
+            {
+                try
+                {
+                    adminApprovalPresidents = ctx.AdminApprovalPresident.FirstOrDefault();
+
+                }
+                catch (Exception ex)
+                {
+                    var stackTrace = new StackTrace(ex, true);
+                    var line = stackTrace.GetFrame(0).GetFileLineNumber();
+                    Logger.LogExceptions(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, ex.Message, line.ToString(), Logger.logLevel.Exception);
+                }
+                finally
+                {
+                }
+            }
+            Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "Exit Point", Logger.logLevel.Debug);
+
+            return adminApprovalPresidents;
+        }
+
+        public static String UpdateApproverPresident(AdminApprovalPresident adminApprovalPresident)
+        {
+            Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "Entry Point", Logger.logLevel.Info);
+            Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "", Logger.logLevel.Debug);
+
+            String result = "";
+
+            using (var ctx = new CPPDbContext())
+            {
+                try
+                {
+                    AdminApprovalPresident retrivedAdminApprovalPresident = new AdminApprovalPresident();
+                    retrivedAdminApprovalPresident = ctx.AdminApprovalPresident.Where(p => p.ID.Equals(adminApprovalPresident.ID)).FirstOrDefault();
+
+                    if (retrivedAdminApprovalPresident.Equals(null))
+                    {
+                        result += "President ID: " + adminApprovalPresident.ID + ", could not be found.\n";
+                        //throw new Exception(result);
+                    }
+
+                    else if (retrivedAdminApprovalPresident != null)
+                    {
+                        //  ctx.Entry(myEmp).CurrentValues.SetValues(employee); Good way to update an object but it will overwrite the existing object
+                        CopyUtil.CopyFields<AdminApprovalPresident>(adminApprovalPresident, retrivedAdminApprovalPresident);
+                        ctx.Entry(retrivedAdminApprovalPresident).State = System.Data.Entity.EntityState.Modified;
+                        ctx.SaveChanges();
+                        result += adminApprovalPresident.ID + " has been updated successfully.\n";
+                    }
+                    else
+                    {
+                        result += adminApprovalPresident.ID + " failed to be updated, it does not exist.\n";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var stackTrace = new StackTrace(ex, true);
+                    var line = stackTrace.GetFrame(0).GetFileLineNumber();
+                    Logger.LogExceptions(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, ex.Message, line.ToString(), Logger.logLevel.Exception);
+                    result = ex.Message;
+                }
+                finally
+                {
+                    Logger.LogDebug(MethodBase.GetCurrentMethod().DeclaringType.ToString(), MethodBase.GetCurrentMethod().Name, "Exit Point", Logger.logLevel.Debug);
+                }
+            }
+            return result;
+        }
+    }
+}
