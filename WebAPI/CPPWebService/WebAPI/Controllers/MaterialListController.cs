c73242623bb59643bccba7bc519b8601fcbbac0a
vaishnavi.shinde <vaishnavi.shinde@softlabsgroup.com>
2022-10-27 15:05:49 +0530

    changes for factoring multiple activities - material adding - same element

---
+++ WebAPI/CPPWebService/WebAPI/Controllers/MaterialListController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+namespace WebAPI.Controllers
+{
+    public class MaterialListController : ApiController
+    {
+        public HttpResponseMessage Get(int? projectId = null,int? activityId=null)
+        {
+            if (projectId != null)
+            {
+                BillOfMaterial materiallist = BillOfMaterial.GeMaterialList((int)projectId,(int)activityId);
+
+                var jsonNew = new
+                {
+                    result = "success",
+                    data = materiallist,
+                };
+                return Request.CreateResponse(HttpStatusCode.OK, jsonNew);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
